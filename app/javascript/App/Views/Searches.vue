7122c17c1999e4419c7238e7f4a996ebac2cd3d2
Lucas de Amystherdam <pro.lucas07@gmail.com>
2024-05-15 08:03:26 -0300

    work with mocked data

--- app/javascript/App/Views/Searches.vue
+++ app/javascript/App/Views/Searches.vue
@@ -1,42 +1,53 @@
 <template>
   <div class="h-full flex flex-row flex-wrap items-center mt-4 gap-5">
     <CardDescription
+      v-for="search in searches"
+      :key="search.id"
       imageWidth="w-[400px]"
       imageHeight="h-[300px]"
-      imageLink="https://i.redd.it/xg9q5lxplln11.png"
-      imageDescription="imageDescription"
-    />
-    <CardDescription
-      imageWidth="w-[400px]"
-      imageHeight="h-[300px]"
-      imageLink="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Flag_of_Brazil.svg/1200px-Flag_of_Brazil.svg.png"
-      imageDescription="imageDescription"
-    />
-
-    <CardDescription
-      imageWidth="w-[400px]"
-      imageHeight="h-[300px]"
-      imageLink="https://s1.static.brasilescola.uol.com.br/be/2021/11/1-mapa-brasil.jpg"
-      imageDescription="imageDescription"
-    />
-    <CardDescription
-      imageWidth="w-[400px]"
-      imageHeight="h-[300px]"
-      imageLink="https://www.saibamais.net/wp-content/uploads/2014/10/artes-marciais.jpg"
-      imageDescription="imageDescription"
-    />
-    <CardDescription
-      imageWidth="w-[400px]"
-      imageHeight="h-[300px]"
-      imageLink="https://i.redd.it/xg9q5lxplln11.png"
-      imageDescription="imageDescription"
+      :imageLink="search.imageLink"
+      :imageDescription="search.imageDescription"
     />
   </div>
 </template>
 
 <script>
 import CardDescription from "../Components/CardDescription.vue";
+
 export default {
+  name: "Searches",
+  data() {
+    return {
+      searches: [
+        {
+          id: 1,
+          imageLink: "https://i.redd.it/xg9q5lxplln11.png",
+          imageDescription: "imageDescription",
+        },
+        {
+          id: 2,
+          imageLink: "https://i.redd.it/xg9q5lxplln11.png",
+          imageDescription: "imageDescription",
+        },
+        {
+          id: 3,
+          imageLink: "https://i.redd.it/xg9q5lxplln11.png",
+          imageDescription: "imageDescription",
+        },
+        {
+          id: 4,
+          imageLink:
+            "https://www.saibamais.net/wp-content/uploads/2014/10/artes-marciais.jpg",
+          imageDescription: "batatinha",
+        },
+        {
+          id: 5,
+          imageLink: "https://i.redd.it/xg9q5lxplln11.png",
+          imageDescription: "imageDescription",
+        },
+      ],
+    };
+  },
   components: {
     CardDescription,
   },
